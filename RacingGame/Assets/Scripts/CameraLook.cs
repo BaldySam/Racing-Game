ac1d685821c2a94ce510d004304921c10bb69026
BaldySam <147501901+BaldySam@users.noreply.github.com>
2025-03-04 18:17:32 +0000

    Added in cars
    Added in both the player and ai cars

---
+++ RacingGame/Assets/Scripts/CameraLook.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class CameraLook : MonoBehaviour
+{
+    [Header("Mouse")]
+    [SerializeField] private float mouseX;
+    [SerializeField] private float mouseY;
+
+    [Header("Camera")]
+    [SerializeField] private GameObject playerHead;
+    [SerializeField] private Vector2 xClamp;
+    [SerializeField] private Vector2 yClamp;
+
+    [Header("Camera Sensitivity")]
+    [SerializeField] private float sensitivity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RotateCam();
+    }
+
+    void RotateCam()
+    {
+        mouseX += Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
+        mouseY -= Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
+        mouseY = Mathf.Clamp(mouseY, yClamp.x, yClamp.y);
+        mouseX = Mathf.Clamp(mouseX, xClamp.x, xClamp.y);
+        playerHead.transform.localRotation = Quaternion.Euler(mouseY, mouseX, 0);
+    }
+}
